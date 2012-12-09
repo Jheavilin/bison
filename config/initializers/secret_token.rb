8d32d1ac66a3a247799c7b41cda1bccf9c2b1771
jheavilin <jheavilin@gmail.com>
2012-12-09 10:52:31 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bison::Application.config.secret_token = 'c220070c56fe733e68d63c0502a94ce2b1de975ff1ab83e0afc03d497fa15ce5b0befae54e9c19cf1f50a332bdbea978b3ec5222a8c28cb475bbdb51d10193b2'
